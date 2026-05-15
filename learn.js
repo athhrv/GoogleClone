7f9515c718881457f149f9be5b008478d9f56e45
ather ahmad <mdather385@gmail.com>
2026-05-15 20:11:23 +0530

    added reaadmeFile

--- learn.js
+++ learn.js
@@ -1,1 +1,17 @@
-console.log("athev ahmad")
+// console.log("athev ahmad")
+// const num =2
+// let atherv= "kido"
+// aarav = "atherv ka dos"
+// console.table({ atherv, aarav,num })
+console.log(3 + 3); console.log(4+5)
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true / false
+// null => standalone value
+// undefined =>
+// symbol => define unique components
+
+console.log(typeof 45)
+console.log(typeof null)
